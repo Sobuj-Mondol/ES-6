79a78dc33d2b365f2f83be84d0b3cf9cc07dd30b
Sobuj <sobujkumar424@gmail.com>
2022-02-19 13:05:39 +0600

    simple-recap

---
+++ simple-recap.js
@@ -0,0 +1,49 @@
+// variable
+var deposit = 400;
+// condition
+if(deposit > 500){
+
+}
+else if( deposit < 500){
+
+}
+else if( deposit == 500){
+
+}
+else if( deposit != 500){
+
+}
+else if( deposit >= 500){
+
+}
+else if( deposit <= 500){
+
+}
+else{
+
+}
+// array
+let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let numberCiunt = number.length;
+menuba.pop();
+number.push();
+number[2] = 20;
+// check whether 222 included in the array 
+if(number.indextOf(222) != -1){
+
+}
+if(number.includes(222)){
+
+}
+// loop
+// while,for 
+for(let number of numbers){
+
+}
+// function
+function fullName(first,second){
+    let name = first+' ' + second;
+}
+fullName('Sobuj','Mondol');
+// Object
+let leptop = {color: 'silvar', price: 10000, strong:'1000gb', ram: '4gb'}
